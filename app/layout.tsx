c58834d074a62872450e4dc1f639de73e6b64fbc
Thomas King <thomasgking42@gmail.com>
2025-02-02 19:36:48 +0000

    build new issue page

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       >
         <Theme>
           <NavBar />
-          <main>
+          <main className="p-5">
           {children}
           </main>
         </Theme>
