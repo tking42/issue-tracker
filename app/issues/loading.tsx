6bd3762dfc38dc721aac57b754bb7ff5ace44ae4
Thomas King <thomasgking42@gmail.com>
2025-02-06 18:19:59 +0000

    add loading page

---
+++ app/issues/loading.tsx
@@ -0,0 +1,11 @@
+import { Table } from '@radix-ui/themes'
+import React from 'react'
+
+const LoadingIssuesPage = () => {
+   
+  return (
+   <div>Loading...</div>
+  )
+}
+
+export default LoadingIssuesPage
