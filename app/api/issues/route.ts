ae14d656b01841a959c20dbb3cddf6aebd5bdef8
Thomas King <thomasgking42@gmail.com>
2025-02-17 14:49:00 +0000

    style issue detail page

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -7,7 +7,6 @@ export async function POST(request: NextRequest) {
     const validation = createIssueSchema.safeParse(body)
     if (!validation.success) 
         return NextResponse.json(validation.error.format(), {status: 400})
-
     const newIssue = await prisma.issue.create({
         data: {title: body.title, description: body.description}
     })
