309535bcb86cd72e702b4ed224fe4ebbca12393c
Thomas King <thomasgking42@gmail.com>
2025-02-17 16:02:26 +0000

    api for updating issues

--- app/components/IssueForm.tsx
+++ app/components/IssueForm.tsx
@@ -8,19 +8,19 @@ import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { createIssueSchema } from '@/app/validationSchemas';
+import { issueSchema } from '@/app/validationSchemas';
 import {z} from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
 import { Issue } from '@prisma/client';
 
-type IssueFormData = z.infer<typeof createIssueSchema>
+type IssueFormData = z.infer<typeof issueSchema>
 
 const IssueForm = ({issue}: {issue?: Issue}) => {
 
   const router = useRouter()
   const {register, control, handleSubmit, formState: { errors }} = useForm<IssueFormData>({
-    resolver: zodResolver(createIssueSchema)
+    resolver: zodResolver(issueSchema)
   })
   const [error, setError] = useState('')
   const [isSubmitting, setIsSubmitting] = useState(false)
